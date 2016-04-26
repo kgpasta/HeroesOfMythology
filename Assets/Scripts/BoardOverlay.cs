34a132d1b80441ccd40d7be6b2eec70b21b33b45
kgpasta <kaustubh.garimella@gmail.com>
2016-04-25 22:48:26 -0700

    Added animations, and moved BoardOverlay to seperate class

---
+++ Assets/Scripts/BoardOverlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class BoardOverlay
+    {
+
+        public static void HighlightTiles(List<GameTile> tiles)
+        {
+            List<GameTile> allTiles = Utility.GetBoard().GetAllTiles();
+
+            foreach (GameTile tile in allTiles)
+            {
+                tile.TileObject.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+
+            if(tiles != null)
+            {
+                foreach (GameTile rangeTile in tiles)
+                {
+                    rangeTile.TileObject.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+            }
+        }
+    }
+}
