fde51f1b3c367a4faecd512ce6ebf356d9f6bbed
kgpasta <kaustubh.garimella@gmail.com>
2016-06-20 17:27:43 -0400

    weapon shell code

---
+++ Assets/Scripts/DataModels/WeaponStats.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.DataModels
+{
+    class WeaponStats : MonoBehaviour
+    {
+        public int Attack;
+        public int Durability;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void LoadBaseStats(WeaponBaseStats BaseStats)
+        {
+            Attack = BaseStats.Attack;
+            Durability = BaseStats.Durability;
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/WeaponBaseStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace Assets.Scripts.ScriptableObjects
+{
+    class WeaponBaseStats : ScriptableObject
+    {
+        public int Attack;
+        public int Durability;
+
+        public void Load(string line)
+        {
+            string[] elements = line.Split(',');
+            name = elements[0];
+            Attack = Convert.ToInt32(elements[1]);
+            Durability = Convert.ToInt32(elements[2]);
+        }
+    }
+}
